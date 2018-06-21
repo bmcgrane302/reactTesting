30f1d9f4f6ca5e8121bca5d83f62c5de756a3a2e
bmcgrane302 <bmcgrane302@yahoo.com>
2018-06-21 15:55:55 -0700

    install enzyme

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 
 import App from '../App';
+import CommentBox from '../CommentBox';
 
 
 it('shows a comment box', () =>{
-  const div = document.createElement('div');
-
-  ReactDOM.render(<App />, div);
-
-  console.log(div.innerHTML)
-
-  ReactDOM.unmountComponentAtNode(div);
+ const wrapped = shallow(<App />);
+ expect(wrapped.find(CommentBox).length).toEqual(1);
 });
